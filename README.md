d6e8a6f6625164c0e4235803acb2f20a3f9f58b2
ployt0 <25666053+ployt0@users.noreply.github.com>
2023-07-31 12:26:07 +0100

    use firefox headless

---
+++ README.md
@@ -0,0 +1,5 @@
+![example workflow](https://github.com/ployt0/headless_selenium/actions/workflows/CI.yml/badge.svg)
+
+I have some private projects running Selenium. I want a sensible base project demonstrating Selenium to base these other projects' CI scripts off.
+
+I also need tests for my website. So that's what purpose particular repository can serve.
---
+++ tests/test_site.py
@@ -0,0 +1,39 @@
+import os
+import pytest
+
+from selenium import webdriver
+
+# Use the following lot for headless environments:
+from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+
+
+@pytest.fixture
+def driver():
+    """
+    todo This'll need adjusting per user to run locally.
+    :return:
+    """
+    opts = FirefoxOptions()
+    if os.name == 'nt':
+        # Hardcode your firefox path here. like "AppData\Local\Mozilla Firefox\firefox.exe".
+        # I don't know what shell you use in Windows.
+        opts.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+        service = Service(r"C:\geckodriver.exe")  # Or wherever you keep it.
+        driver = webdriver.Firefox(service=service, options=opts)
+    else:
+        opts.add_argument("--headless")
+        driver = webdriver.Firefox(options=opts)
+    return driver
+
+
+def test_site(driver):
+    try:
+        driver.implicitly_wait(10)
+        driver.get("https://silverbullets.co.uk")
+        driver.find_element(By.XPATH, '//article//header/h2/a').click()
+        assert driver.find_elements(By.CSS_SELECTOR, "div .col-md-8")[0].text.strip().startswith("Published 20")
+    finally:
+        driver.close()
+        driver.quit()
